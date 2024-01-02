d0b8706969839b61828ae773858fdbc7f34eed5f
MikuroXina <ryosukadnak@gmail.com>
2024-01-02 17:30:39 +0900

    Split loadOrGenerateKeyPair as new file

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,7 @@
 import config from "../config.json" assert { type: "json" };
 import { Hono } from "hono";
 import { SignJWT } from "jose";
-
-const KEY_GEN_ALGORITHM = {
-    name: "RSASSA-PKCS1-v1_5",
-    modulusLength: 2048,
-    publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-    hash: { name: "SHA-256" },
-};
-
-const KEY_IMPORT_ALGORITHM = {
-    name: "RSASSA-PKCS1-v1_5",
-    hash: { name: "SHA-256" },
-};
-
-async function loadOrGenerateKeyPair(store: KVNamespace) {
-    const keyPairJson = await store.get<{
-        publicKey: ArrayBuffer | JsonWebKey;
-        privateKey: ArrayBuffer | JsonWebKey;
-    }>("keys", { type: "json" });
-
-    if (keyPairJson === null) {
-        const keyPair = (await crypto.subtle.generateKey(
-            KEY_GEN_ALGORITHM,
-            true,
-            ["sign", "verify"],
-        )) as CryptoKeyPair;
-        const privateKey = await crypto.subtle.exportKey(
-            "jwk",
-            keyPair.privateKey,
-        );
-        const publicKey = await crypto.subtle.exportKey(
-            "jwk",
-            keyPair.publicKey,
-        );
-        await store.put("keys", JSON.stringify({ privateKey, publicKey }));
-        return keyPair;
-    }
-
-    return {
-        publicKey: await crypto.subtle.importKey(
-            "jwk",
-            keyPairJson.publicKey,
-            KEY_IMPORT_ALGORITHM,
-            true,
-            ["verify"],
-        ),
-        privateKey: await crypto.subtle.importKey(
-            "jwk",
-            keyPairJson.privateKey,
-            KEY_IMPORT_ALGORITHM,
-            true,
-            ["sign"],
-        ),
-    };
-}
+import { loadOrGenerateKeyPair } from "./key";
 
 const DISCORD_API_ROOT = "https://discord.com/api/v10";
 
