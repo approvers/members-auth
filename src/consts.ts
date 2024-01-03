79302d3148c78c74982f8703b400e2a9fb1de1b4
MikuroXina <ryosukadnak@gmail.com>
2024-01-03 17:36:22 +0900

    Add consts

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const DISCORD_CLIENT_ID = "1191642657731121272";
+export const DISCORD_API_ROOT = "https://discord.com/api/v10";
+
+export const CLOUDFLARE_ACCESS_REDIRECT_URI =
+    "https://approvers.cloudflareaccess.com/cdn-cgi/access/callback";
